a97ddb52698c35e3938ca0e5d846724b363d5093
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2024-08-02 00:24:12 +0300

    initial - initial setup - add implementation of test lambda function

---
+++ lambda/main.go
@@ -0,0 +1,23 @@
+// Contains the backend logic for the app
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Username string `json:"username"`
+}
+
+func HandleRequest(event MyEvent) (string, error) {
+	if event.Username == "" {
+		return "", fmt.Errorf("username cannot be empty")
+	}
+	return fmt.Sprintf("successfully called by - %s", event.Username), nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
