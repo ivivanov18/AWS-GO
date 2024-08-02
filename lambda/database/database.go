df93c4bb9a055bd11496b3b335df7d297d8aa12e
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2024-08-03 02:49:31 +0300

    db: - add possibility to get users

--- lambda/database/database.go
+++ lambda/database/database.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"fmt"
 	"lambda-func/types"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -69,4 +70,31 @@ func (client DynamoDbClient) InsertUser(user types.RegisterUser) error {
 	}
 
 	return nil
+}
+
+func (client DynamoDbClient) GetUser(username string) (types.User, error) {
+	var user types.User
+
+	result, err := client.databaseStore.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Key: map[string]*dynamodb.AttributeValue(
+			"username": {
+				S: aws.String(username),
+			},
+		),
+	})
+
+	if err != nil {
+		return user, err
+	}
+
+	if result.Item == nil {
+		return user, fmt.Errorf("user not found")
+	} 
+
+	err = dynamodb.dynanodbattribute.UnmarshalMap(result.Item, &user)
+	if err != nil {
+		return user, err
+	} 
+	return user, nil
 }
